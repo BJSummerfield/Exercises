cd1d1ec056dda594a6f911e57bfb2150bf8d6547
Brian Summerfield <brianjsummerfield@gmail.com>
2019-04-15 22:36:05 -0500

    fix roman method

--- change_machine.rb
+++ change_machine.rb
@@ -15,15 +15,19 @@ class ChangeMachine
   def roman(number)
     answer = []
     while number > 0
-      answer << "M"  && number -= 1000 if number >= 1000 && number > 501
-      answer << "D"  && number -= 500 if number >= 500 && number > 101
-      answer << "C"  && number -= 100 if number >= 100 && number > 51
-      answer << "L"  && number -= 50 if number >= 50 && number > 26
-      answer << "X"  && number -= 10 if number < 25 && number > 11
-      answer << "VI"  && number -= 6 if number == 6
-      answer << "V"  && number -= 5 if number < 10 && number > 4
-      answer << "IV"  && number -= 4 if number == 4
-      answer << "I"  && number -= 1 if number < 5 && number > 0
+      answer << "M"  && number -= 1000 if number >= 1000 
+      answer << "CM"  && number -= 900 if number >= 900 && number < 1000
+      answer << "D"  && number -= 500 if number >= 500 && number < 900
+      answer << "CD"  && number -= 400 if number >= 400 && number < 500
+      answer << "C"  && number -= 100 if number >= 100 && number < 400
+      answer << "XC"  && number -= 90 if number >= 90 && number < 100
+      answer << "L"  && number -= 50 if number >= 50 && number < 90
+      answer << "XL"  && number -= 40 if number >= 40 && number < 50
+      answer << "X"  && number -= 10 if number >= 10 && number < 40
+      answer << "IX"  && number -= 9 if number >= 9 && number < 10
+      answer << "V"  && number -= 5 if number >= 5 && number < 9 
+      answer << "IV"  && number -= 4 if number >= 4 && number < 5
+      answer << "I"  && number -= 1 if number > 0 && number < 4 
     end
     return answer.join('')
   end
@@ -35,7 +39,7 @@ RSpec.describe ChangeMachine do
   describe '#roman' do
     it 'should return [VIV] when given 9' do
       machine = ChangeMachine.new
-      expect(machine.roman(9)).to eq("VIV")
+      expect(machine.roman(9)).to eq("IX")
     end
   end
   describe '#roman' do
